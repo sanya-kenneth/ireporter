3c2fe7e01590947bc1a2950196305a36fa497b61
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-27 18:20:59 +0300

    modify user routes and models, refactor in views.py and models.py

--- api/auth/models.py
+++ api/auth/models.py
@@ -2,6 +2,9 @@ import uuid
 import datetime
 
 
+user_db = []
+
+
 class User:
     def __init__(self, firstname, lastname, othernames,
                  email, phoneNumber, username, password):
@@ -30,8 +33,39 @@ class User:
                  "isAdmin": self.isAdmin
                }
 
+    def check_user_exists(self, email):
+        for user in user_db:
+            return user['email'] == email
+
 
 class Admin(User):
-    def __init__(self, *args, **kwargs):
-        User.__init__(self, *args, **kwargs)
+    def __init__(self, firstname, lastname, othernames,
+                 email, phoneNumber, username, password):
+        self.user_id = uuid.uuid4()
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othernames = othernames
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.username = username
+        self.password = password
+        self.registered = datetime.datetime.now()
         self.isAdmin = True
+
+    def to_json(self):
+        return {
+                 "user_id": str(self.user_id.int)[:10],
+                 "firstname": self.firstname,
+                 "lastname": self.lastname,
+                 "othername": self.othernames,
+                 "email": self.email,
+                 "phoneNumber": self.phoneNumber,
+                 "username": self.username,
+                 "userpassword": self.password,
+                 "registered": self.registered,
+                 "isAdmin": self.isAdmin
+               }
+
+    def check_admin_isthere(self, email):
+        for admin in user_db:
+            return admin['email'] == email
--- api/auth/utilities.py
+++ api/auth/utilities.py
@@ -1,4 +1,11 @@
+from flask import request, jsonify, current_app as app
+from validate_email import validate_email
+from api.auth.models import User, Admin, user_db
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 import re
+import jwt
+import datetime
 
 
 class validateUser:
@@ -14,3 +21,103 @@ class validateUser:
     def validate_password(password):
         return isinstance(password, str) and len(password) >= 8 and \
                 re.search(r'[A-Z]', password) and re.search(r'[0-9]', password)
+
+
+def protected(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'token' in request.headers:
+            token = request.headers['token']
+        if not token:
+            return jsonify({'status': 401, 'error': 'Token is missing'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET'], algorithms=['HS256'])
+            for user_info in user_db:
+                if user_info['email'] == data['user']:
+                    current_user = user_info
+        except:
+            return jsonify({'status': 401, 'error': 'Token is invalid'}), 401
+        try:
+            return f(current_user, *args, **kwargs)
+        except:
+            return jsonify({'status': 401, 'error': 'Please login first'}), 401
+    return decorated
+
+
+def check_is_admin(current_user):
+    return current_user['isAdmin']
+
+
+def signup_user(user_type):
+    data = request.get_json()
+    firstName = data.get('firstname')
+    lastName = data.get('lastname')
+    otherNames = data.get('othernames')
+    email = data.get('email')
+    phoneNumber = data.get('phonenumber')
+    userName = data.get('username')
+    userPassword = data.get('password')
+    if not firstName or not lastName or not\
+            otherNames or not email or not phoneNumber or not \
+            userName or not userPassword:
+        return jsonify({'status': 400,
+                        'error': 'A required field is either missing or empty'
+                        }), 400
+    if not validateUser.validate_names(firstName) or not \
+            validateUser.validate_names(lastName) or not \
+            validateUser.validate_names(otherNames) or not \
+            validateUser.validate_names(userName):
+        return jsonify({'status': 400,
+                        'error': 'Name must be a string and must not contain spaces'
+                        }), 400
+    if not validateUser.validate_phoneNumber(phoneNumber):
+        return jsonify({'status': 400,
+                        'error': 'Only numbers are allowed for the phonenumber field'
+                        }), 400
+    if not validate_email(email):
+        return jsonify({'status': 400, 'error': 'Invalid email'}), 400
+    if not validateUser.validate_password(userPassword):
+        return jsonify({'status': 400,
+                        'error': 'Password must be atleast 8 characters and should have atleast one number and one capital letter'}), 400
+    if user_type == 'user':
+        user = User(firstName, lastName, otherNames, email,
+                    phoneNumber, userName, generate_password_hash(userPassword))
+        if user.check_user_exists(email):
+            return jsonify({'status': 400,
+                            'error': 'User account already exists'}), 400
+    elif user_type == 'admin':
+        user = Admin(firstName, lastName, otherNames, email,
+                     phoneNumber, userName, generate_password_hash(userPassword))
+        if user.check_admin_isthere(email):
+            return jsonify({'status': 400,
+                            'error': 'Admin account already exists'}), 400
+    else:
+        return jsonify({'error': 'invalid user'}), 400
+    user_db.append(user.to_json())
+    return jsonify({'status': 201, 'data': user.to_json(),
+                    'message': 'Your Account was created successfuly'}), 201
+
+
+def login_user():
+    login_info = request.get_json()
+    login_email = login_info.get('email')
+    login_password = login_info.get('password')
+    if not login_email or not login_password:
+        return jsonify({'status': 400,
+                        'error': 'email or password cannot be empty'}), 400
+    if not validate_email(login_email):
+        return jsonify({'status': 400, 'error': 'Invalid email'}), 400
+    if not validateUser.validate_password(login_password):
+        return jsonify({'status': 400,
+                        'error': 'Password must be atleast 8 characters and should have atleast one number and one capital letter'}), 400
+    for search_data in user_db:
+        if search_data['email'] == login_email and \
+             check_password_hash(search_data['userpassword'], login_password):
+            token = jwt.encode({'user': search_data['email'],
+                                'exp': datetime.datetime.utcnow()+datetime.timedelta(hours=24)},
+                               app.config['SECRET'],
+                               algorithm='HS256')
+            return jsonify({'status': 200, 'token': token.decode('UTF-8'),
+                            'message': 'You are now loggedin'}), 200
+    return jsonify({'satus': 401, 'error': 'Wrong email or password'}), 401
