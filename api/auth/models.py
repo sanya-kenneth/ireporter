e942a9ca79f4da23a8838f6dfc37e1cbfb395e48
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-28 15:56:49 +0300

    modify user models

--- api/auth/models.py
+++ api/auth/models.py
@@ -40,15 +40,15 @@ class User:
 
 
 class Admin(User):
-    def __init__(self, firstname, lastname, othernames,
-                 email, phoneNumber, username, password):
+    def __init__(self, fname, lname, admin_othernames,
+                 admin_email, admin_mobile_number, admin_username, admin_password):
         self.user_id = uuid.uuid4()
-        self.firstname = firstname
-        self.lastname = lastname
-        self.othernames = othernames
-        self.email = email
-        self.phoneNumber = phoneNumber
-        self.username = username
-        self.password = password
+        self.firstname = fname
+        self.lastname = lname
+        self.othernames = admin_othernames
+        self.email = admin_email
+        self.phoneNumber = admin_mobile_number
+        self.username = admin_username
+        self.password = admin_password
         self.registered = datetime.datetime.now()
         self.isAdmin = True
