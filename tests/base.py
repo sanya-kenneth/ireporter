2575c306dd39529bbec5e2a64c9cf08816713b89
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-28 04:06:48 +0300

    modify tests/base.py

--- tests/base.py
+++ tests/base.py
@@ -2,6 +2,7 @@ from api.incident.models import incident_db
 from api.auth.models import user_db
 from api import create_app
 import unittest
+import json
 
 
 class BaseTest(unittest.TestCase):
@@ -16,6 +17,46 @@ class BaseTest(unittest.TestCase):
     def tearDown(self):
         user_db.clear()
         incident_db.clear()
+
+    def get_token_admin(self):
+        admin_data = {
+                    "firstname":"kenneth",
+                    "lastname":"sanya",
+                    "othernames":"ken",
+                    "email":"sanya@gmail.com",
+                    "phonenumber":256706578719,
+                    "username":"skimo",
+                    "password":"qs1szwwwaAwx"
+                    }
+        admin_data_login = {
+                            "email":"sanya@gmail.com",
+                            "password": "qs1szwwwaAwx"
+                            }
+        self.app.post('/api/v1/users/admin', content_type="application/json", data=json.dumps(admin_data))
+        res = self.app.post('/api/v1/users/login', content_type="application/json", data=json.dumps(admin_data_login))
+        data = json.loads(res.data.decode())
+        return data['token']
+
+
+    def get_token_user(self):
+        user_data = {
+                    "firstname":"len",
+                    "lastname":"leno",
+                    "othernames":"ken",
+                    "email":"len@gmail.com",
+                    "phonenumber":256786578719,
+                    "username":"len",
+                    "password":"1awQdddddd"
+                    }
+        user_data_login = {
+                            "email":"len@gmail.com",
+                            "password": "1awQdddddd"
+                            }
+        self.app.post('/api/v1/users', content_type="application/json", data=json.dumps(user_data))
+        res = self.app.post('/api/v1/users/login', content_type="application/json", data=json.dumps(user_data_login))
+        data = json.loads(res.data.decode())
+        return data['token']
+
 
 
 if __name__ == '__main__':
