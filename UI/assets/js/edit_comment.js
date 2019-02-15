af86c8c69f71632460748425b4210f7642a74d2a
sanya-kenneth <sanyakenneth@gmail.com>
2019-02-15 19:11:29 +0300

    modify edit comment functionality

--- UI/assets/js/edit_comment.js
+++ UI/assets/js/edit_comment.js
@@ -7,6 +7,14 @@ let urlEdit = (recordType) => {
     }
 }
 
+function remove_error(){
+    document.getElementById("edit_comment_error_box").style.display = 'none';
+}
+
+function remove_message(){
+    document.getElementById("edit_comment_message_box").style.display = 'none';
+}
+
 
 const editComment = (event) => {
     event.preventDefault();
@@ -28,7 +36,30 @@ const editComment = (event) => {
     })
     .then((res) => res.json())
     .then((data) => {
-        console.log(data);
-    })
+        if (data.message == `Updated ${newrecordCurrentType} record's comment`){
+            let comment_form = document.getElementById("edit_comment_form");
+            let edit_comment_message_box = 
+            document.getElementById("edit_comment_message_box");
+            edit_comment_message_box.innerHTML = data.message;
+            edit_comment_message_box.style.display = 'block';
+            setTimeout(remove_message, 3000);
+            comment_form.reset();
+  
+        }
+        else if(data.message === "incident record not found"){
+            let edit_comment_error_box = 
+            document.getElementById("edit_comment_error_box");
+            edit_comment_error_box.innerHTML = data.message;
+            console.log(data.message)
+            edit_comment_error_box.style.display = 'block';
+            setTimeout(remove_error, 3000);
+        }
+        else{
+            edit_comment_error_box.innerHTML = data.error;
+            console.log(data.error)
+            edit_comment_error_box.style.display = 'block';
+            setTimeout(remove_error, 3000);
+        }
+    });
 
 }
