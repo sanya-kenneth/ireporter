c2cadadce5b5072027f355743e8b64094cbf4f6c
sanya-kenneth <sanyakenneth@gmail.com>
2019-02-23 00:04:43 +0300

    Add functionality to get a user

--- UI/assets/js/user_profile.js
+++ UI/assets/js/user_profile.js
@@ -1,3 +1,14 @@
 const fetchUserDetails = () => {
-    fetch()
+    fetch('http://127.0.0.1:5000/api/v1/user', 
+    {
+        method: 'GET',
+        headers: {
+                    'Content-type': 'application/json',
+                    "Authorization": sessionStorage.getItem("access_token"),
+                }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
 }
